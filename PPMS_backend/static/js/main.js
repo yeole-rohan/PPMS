7de9b8f8bee995ff9c2cf56719f85ab597ceb45d
rohan-y <rohan.y@media.net>
2019-07-21 17:14:39 +0530

    added dynamic chart for diesel

--- PPMS_backend/static/js/main.js
+++ PPMS_backend/static/js/main.js
@@ -7,21 +7,67 @@ $.ajax({
     success:function(data){
         labels = data.labels
         defaultData = data.defaultDatas
-        setChart()
+            var ctx = document.getElementById('myChart1').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: labels ,
+                        datasets: [{
+                            label: 'Sale Per Day For Petrol in Rupees',
+                            data: defaultData,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
     },
     error:function(error){
         console.log("error")
         console.log(error)
     }
 });
-function setChart(){
-    var ctx = document.getElementById('myChart1').getContext('2d');
+
+
+var endPoint = 'api/res/diesel-profit'
+var totalAmount = []
+var labels = []
+$.ajax({
+    methos:"GET",
+    url:endPoint,
+    success:function(data){
+        labels = data.date
+        defaultData = data.amount
+        var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: labels ,
                 datasets: [{
-                    label: 'Sale Per Day For Petrol in Rupees',
+                    label: 'Sale Per Day For Diesel in Rupees',
                     data: defaultData,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
@@ -52,60 +98,9 @@ function setChart(){
                 }
             }
         });
-}
-
-// var endPoint = 'api/res/diesel-profit'
-// var totalAmount = []
-// var labels = []
-// $.ajax({
-//     methos:"GET",
-//     url:endPoint,
-//     success:function(data){
-//         labels = data.date
-//         defaultData = data.totalAmount
-//         setChart()
-//     },
-//     error:function(error){
-//         console.log("error")
-//         console.log(error)
-//     }
-// });
-// function setChart(){
-//     var ctx = document.getElementById('myChart').getContext('2d');
-//         var myChart = new Chart(ctx, {
-//             type: 'bar',
-//             data: {
-//                 labels: labels ,
-//                 datasets: [{
-//                     label: 'Sale Per Day For Diesel in Rupees',
-//                     data: defaultData,
-//                     backgroundColor: [
-//                         'rgba(255, 99, 132, 0.2)',
-//                         'rgba(54, 162, 235, 0.2)',
-//                         'rgba(255, 206, 86, 0.2)',
-//                         'rgba(75, 192, 192, 0.2)',
-//                         'rgba(153, 102, 255, 0.2)',
-//                         'rgba(255, 159, 64, 0.2)'
-//                     ],
-//                     borderColor: [
-//                         'rgba(255, 99, 132, 1)',
-//                         'rgba(54, 162, 235, 1)',
-//                         'rgba(255, 206, 86, 1)',
-//                         'rgba(75, 192, 192, 1)',
-//                         'rgba(153, 102, 255, 1)',
-//                         'rgba(255, 159, 64, 1)'
-//                     ],
-//                     borderWidth: 1
-//                 }]
-//             },
-//             options: {
-//                 scales: {
-//                     yAxes: [{
-//                         ticks: {
-//                             beginAtZero: true
-//                         }
-//                     }]
-//                 }
-//             }
-//         });
-// }
+    },
+    error:function(error){
+        console.log("error")
+        console.log(error)
+    }
+});
